c696172061451e6fa41d699121e7109521f7d177
rahul kumar <rahulkumarvirat1510@gmail.com>
2026-04-09 12:22:40 +0000

    updated variable.sh

--- variable.sh
+++ variable.sh
@@ -2,5 +2,13 @@
 name="Rahul"
 role="Devops Engineer"
 
+current_user=$(whoami)
+current_dir=$(pwd)
+today=$(date)
+
 echo "My name is $name"
 echo "My role is $role"
+
+echo "System user: $current_user"
+echo "Directory: $current_dir
+echo "Date: $today"
